7d5fc24243950899be8db01b5f61f96b342322e5
Emily Kelso <elumiakelso@gmail.com>
2025-04-07 20:22:57 -0700

    fix linting errors

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,6 @@
 // const { get } = require("mongoose");
 
 const router = require("express").Router();
-const { get } = require("mongoose");
 const { getUsers, createUser, getUser } = require("../controllers/users");
 
 router.get("/", getUsers);
